faf573ce206002abf98b55f961400f1eb6c9f9ae
Owen Valentinus <owenwijaya89@gmail.com>
2022-10-02 09:51:45 +0800

    categorized oop data saving method, will try to implement data class later

---
+++ src/oop_way_to_store_data/main.py
@@ -0,0 +1,14 @@
+import courses
+import json
+with open("./data/config.json", "r") as file:
+    configs = json.load(file)
+    sender = configs["sender"]
+    password = configs["password"]
+    recipient = configs["recipient"]
+
+all_courses = [courses.C01_C, courses.L04, courses.CE1_0, courses.L03, courses.T02, courses.TE1, courses.C02, courses.T01, courses.CE1_1, courses.C01_J, courses.T90]
+
+for course in all_courses:
+    if course.is_there():
+        course.send_email(sender, password, recipient)
+        course.open_zoom()
